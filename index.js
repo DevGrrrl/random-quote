61da282a1143d8fc80641c60428921ced927cea0
Lex <alexisjaynedefoe@gmail.com>
2017-12-20 20:07:43 +0000

    Revert "add tweet link & icon"
    This reverts commit a726e3f8122eb1e421b1f338745500f10929de5f.

--- index.js
+++ index.js
@@ -4,45 +4,37 @@ var search = document.getElementById('search');
 var section = document.getElementById('quote__block');
 var quote = document.getElementById('quote__text');
 var author = document.getElementById('quote__author');
-var twitterbtn = document.getElementById('twitter-share-button');
 var link;
-var tweetText;
-
-function tweet() {
-
-}
 
 function updateDom(response) {
+  console.log(response);
   //stop repeat of quotes
-  if (response.quoteLink === quote.link) {
+  if(response.quoteLink === quote.link) {
     getData();
   } else {
     //updateDom
     quote.link = response.quoteLink;
     var text = response.quoteText;
-    quote.innerText = "\" " + text.trim() + "\" ";
+    quote.innerText = text;
     author.innerText = response.quoteAuthor;
-    tweetText = "\"" + text.trim() + "\" " + response.quoteAuthor + "   %23quotes";
-    twitterbtn.href = "https://twitter.com/intent/tweet?text=" + tweetText;
-    //only display tweet link once quote quote has been received
-    if (section.classList.contains('hidden')) {
-      section.classList.remove('hidden');
-    }
   }
 
 }
 
-function getData() {
-  var script = document.createElement("script");
-  script.src = "http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=updateDom";
-  document.getElementsByTagName("head")[0].appendChild(script);
+function getData() { var script = document.createElement("script");
+  script.src = "http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=updateDom"; document.getElementsByTagName("head")[0].appendChild(script);
 }
 
 
 function newQuoteListener() {
   search.addEventListener('click', function(e) {
-    getData();
-  })
+    if(section.classList.contains('hidden')) {
+       section.classList.remove('hidden');
+       getData();
+  } else {
+     getData();
+   }
+ })
 }
 
 
